3c9f79ccafa20a972f7caecebceab005f7885da6
Alex Kruglov <lime4512@mail.ru>
2024-02-19 00:17:12 +0300

    add ai bot

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import { Telegraf } from 'telegraf'
-import { TelegramToken } from './config.js'
+import { TelegramToken, RsnChatToken } from './config.js'
+import { RsnChat } from 'rsnchat'
 
 const bot = new Telegraf(TelegramToken)
 
-bot.start(ctx => ctx.reply('Добро пожаловать в мой Бот :))'))
+const rsnchat = new RsnChat(RsnChatToken)
+
+bot.start(ctx =>
+	ctx.reply('Добро пожаловать я Бот, можете задавать мне любые вопросы :))')
+)
+
+bot.on('message', async ctx =>
+	ctx.replyWithChatAction('typing').then(() => {
+		rsnchat.gpt(ctx.message.text).then(response => {
+			ctx.reply(response.message)
+		})
+	})
+)
 
 bot.launch()
